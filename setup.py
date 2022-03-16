cb561c20939712f530e23190a7dd6a2fb93305e6
or's mac <or@cypago.com>
2022-03-16 17:39:43 +0200

    stable server

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from server import run
+import os
 
 if __name__ == '__main__':
     from sys import argv
 
     if len(argv) == 2:
-        run(port=int(argv[1]))
+        run(port=os.environ['PORT'])
     else:
         run()
